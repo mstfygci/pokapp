b1dec8416d9b966fee0d6a4450f3ee85e0354bd6
mstfygci <mustafa.1643@gmail.com>
2024-04-22 08:20:48 +0300

    feat: airbnb and @typescript-eslint eslint config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import {ReactNode} from "react";
+import type { ReactNode } from 'react';
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
