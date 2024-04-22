5b53db91094283b22f254c05cc863aac99958003
mstfygci <mustafa.1643@gmail.com>
2024-04-22 10:11:44 +0300

    feat: reorder tailwind classes on home element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-      <div className="px-6 pt-10 pb-8 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+      <div className="px-6 pb-8 pt-10 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="mx-auto max-w-md">
           <p>Hello, World!</p>
         </div>
