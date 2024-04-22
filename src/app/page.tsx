6180158f29516848a68775ef552fdf2ee0daaa8e
mstfygci <mustafa.1643@gmail.com>
2024-04-22 09:51:48 +0300

    feat: add tailwindcss and postcss dev dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <p>Hello, World!</p>
+    <div className="flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+      <div className="px-6 pt-10 pb-8 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <div className="mx-auto max-w-md">
+          <p>Hello, World!</p>
+        </div>
+      </div>
+    </div>
   );
 }
