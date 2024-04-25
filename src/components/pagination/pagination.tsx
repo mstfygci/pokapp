2574f322daa2f9dbc87caf0c175462b87dc8fc59
mstfygci <mustafa.1643@gmail.com>
2024-04-25 20:11:10 +0300

    feat: add svg icons as asset

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
+import leftArrow from '../../assets/left-arrow.svg';
+import rightArrow from '../../assets/right-arrow.svg';
 import Button from './button';
 
 export default function Pagination({
@@ -35,21 +38,7 @@ export default function Pagination({
         type="button"
         onClick={() => prevPage()}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          aria-hidden="true"
-          className="size-4"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-          />
-        </svg>
+        <Image className="size-4" src={leftArrow} alt="Left Arrow" />
         Previous
       </button>
       <div className="flex items-center gap-2">
@@ -66,21 +55,7 @@ export default function Pagination({
         onClick={() => nextPage()}
       >
         Next
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          aria-hidden="true"
-          className="size-4"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-          />
-        </svg>
+        <Image className="size-4" src={rightArrow} alt="Right Arrow" />
       </button>
     </>
   );
