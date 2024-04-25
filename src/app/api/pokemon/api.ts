cc0e4de75e096918fbde0c0af6a517f60d7283ac
mstfygci <mustafa.1643@gmail.com>
2024-04-25 22:01:27 +0300

    feat: add support api url from env

--- src/app/api/pokemon/api.ts
+++ src/app/api/pokemon/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const pokeApi = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2',
+  baseURL: process.env.NEXT_API_URL ?? 'https://pokeapi.co/api/v2',
   timeout: Number(process.env.NEXT_AXIOS_TIMEOUT) ?? 1000,
 });
 export default pokeApi;
